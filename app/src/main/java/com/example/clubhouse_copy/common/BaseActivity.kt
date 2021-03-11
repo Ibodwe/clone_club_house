0f8a6d937511d51fdad0eb2e6c76142a93be821f
최정우 <choejeong-u@choejeong-uui-MacBookPro.local>
2021-03-11 10:41:35 +0900

    temp 1

--- app/src/main/java/com/example/clubhouse_copy/common/BaseActivity.kt
+++ app/src/main/java/com/example/clubhouse_copy/common/BaseActivity.kt
@@ -6,6 +6,7 @@ import androidx.annotation.LayoutRes
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import androidx.databinding.ViewDataBinding
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelLazy
@@ -32,18 +33,38 @@ abstract class BaseActivity< B : ViewDataBinding, VM : BaseViewModel>(@LayoutRes
         binding = DataBindingUtil.setContentView(this , layoutResId)
 
 
-        binding.apply {
+        binding {
             lifecycleOwner = this@BaseActivity
             setVariable(5 , viewModel)
         }
 
-        viewModel.apply{
+        viewModel{
             liveLoading.observe(this@BaseActivity) {
                 this@BaseActivity.showMessage(it.toString())
             }
         }
-        
+
+
     }
+
+    // action이 함수다 --> 함수 run으로 실행
+    protected fun binding(action: B.() -> Unit){
+        binding.run(action)
+    }
+
+    protected fun viewModel(action : VM.() -> Unit){
+        viewModel.run(action)
+    }
+
+    //infix함수는, 객체와 객체 사이를 연결 해주는 역할을 한다.
+    protected infix fun <T> LiveData<T>.observe(action : (T) -> Unit){
+        observe(this@BaseActivity, action)
+    }
+
+    protected infix fun <T> LiveData<Event<T>>.eventObserve(action: (T) -> Unit){
+        observe(this@BaseActivity, {it.get(action)})
+    }
+
 
 
 }
---
+++ app/src/main/java/com/example/clubhouse_copy/ui/LoginViewModel.kt
@@ -0,0 +1,4 @@
+package com.example.clubhouse_copy.ui
+
+class LoginViewModel {
+}
